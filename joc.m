cd811c1945321c1409b6e7b773aeb732453e010b
Robert-G-Rada <xpertrobert95@gmail.com>
2017-03-17 17:10:02 +0200

    2

--- joc.m
+++ joc.m
@@ -5,12 +5,25 @@ function [] = joc()
     running = 1;
     while (running)
         input = startQuery();
-        
-        if (input == 2)
+
+        if (input == -1)
             break
+        else
+            result = startGame(input);
         end
     end
 end
+
+function result = startGame(player_symbol)
+
+    for turn = 1 : 9
+        if (mod(turn + player_symbol, 1) == 0)
+            playerMove();
+        else
+            computerMove();
+        end
+
+    end
 
 function input = startQuery()
     disp("\n\n");
@@ -29,7 +42,7 @@ function input = startQuery()
         elseif (input == '0' || input == 'o' || input == 'O')
             input = 0;
         elseif (input == 'q' || input == 'Q' || input == 27)
-            input = 2;
+            input = -1;
         else
             stop = 0;
         end
